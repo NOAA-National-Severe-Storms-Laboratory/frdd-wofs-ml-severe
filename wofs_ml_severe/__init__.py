bd52630e1bc9815025d852a29ee36b141fe441ab
monte-flora <monte.flora@noaa.gov>
2023-03-22 10:10:14 -0500

    remove .viz for init

--- wofs_ml_severe/__init__.py
+++ wofs_ml_severe/__init__.py
@@ -2,7 +2,7 @@
 from .io.io import load_ml_data
 from .data_pipeline.ml_data_generator import MLDataGenerator
 from .data_pipeline.ensemble_track_segmentation import generate_ensemble_track_file 
-from .viz.plot_tracks_and_reports import PlotTracksAndReports
+#from .viz.plot_tracks_and_reports import PlotTracksAndReports
 
 
 
